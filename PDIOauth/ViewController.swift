b49f2936bad6fdb5bdd45042674ad7bf68ba5196
Juliane Ferreira <login+JulianeGF@elo7.com>
2022-06-21 21:34:04 -0300

    Cria botao de login

--- PDIOauth/ViewController.swift
+++ PDIOauth/ViewController.swift
@@ -12,10 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+                
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        button.backgroundColor = .black
+        button.setTitle("Login com GitHub", for: .normal)
+        button.addTarget(self, action: #selector(handlePresentingVC(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(button)
     }
-
-
+    
+    @objc func handlePresentingVC(_ sender: UIButton) {
+        
+    }
 }
-
