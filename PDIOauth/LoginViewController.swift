31eced5f0bc5038ae456d4fca4548a018f6d7c7f
Juliane Ferreira <login+JulianeGF@elo7.com>
2022-06-23 10:13:53 -0300

    adiciona webview

---
+++ PDIOauth/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  PDIOauth
+//
+//  Created by developer on 22/06/22.
+//
+
+import Foundation
--- PDIOauth/ViewController.swift
+++ PDIOauth/ViewController.swift
@@ -1,28 +1,30 @@
-//
-//  ViewController.swift
-//  PDIOauth
-//
-//  Created by developer on 21/06/22.
-//
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+// Usar safari services
+
+class LoginViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .gray
-                
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+                        
+        let button = UIButton(frame: CGRect(x: 100, y: 700, width: 200, height: 50))
         button.backgroundColor = .black
         button.setTitle("Login com GitHub", for: .normal)
-        button.addTarget(self, action: #selector(handlePresentingVC(_:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handlePresentingVC), for: .touchUpInside)
         
         self.view.addSubview(button)
     }
     
-    @objc func handlePresentingVC(_ sender: UIButton) {
-        
+    @objc func handlePresentingVC() {
+        let url = URL(string: "https://www.github.com/login")!
+        webView.load(URLRequest(url: url))
     }
 }
